80f387f2fb93351663978cfc58b97ba92d6b6084
ruucm <ruucm.a@gmail.com>
2018-09-01 04:44:11 +0900

    add top

--- code/Animate.tsx
+++ code/Animate.tsx
@@ -35,19 +35,21 @@ export class Animate extends React.Component<Props> {
   // Set default properties
   static defaultProps = {
     text: 'Animate',
-    left: 100,
-    scale: 2.6,
+    left: 50,
+    top: 50,
+    scale: 2,
     tension: 500,
     friction: 100,
     children: null,
   }
 
-  switch = PropertyStore({ left: 0, scale: 1 }, true)
+  switch = PropertyStore({ left: 0, top: 0, scale: 1 }, true)
 
   // Items shown in property panel
   static propertyControls: PropertyControls = {
     text: { type: ControlType.String, title: 'Text' },
-    left: { type: ControlType.Number, title: 'Left' },
+    left: { type: ControlType.Number, title: 'X (From Left)' },
+    top: { type: ControlType.Number, title: 'Y (From Top)' },
     scale: { type: ControlType.Number, title: 'Scale' },
     tension: { type: ControlType.Number, title: 'Tension' },
     friction: { type: ControlType.Number, title: 'Friction' },
@@ -62,9 +64,10 @@ export class Animate extends React.Component<Props> {
       }
 
       const left = this.props.left
+      const top = this.props.top
       const scale = this.props.scale
 
-      animate.spring(this.switch, { left, scale }, springOptions)
+      animate.spring(this.switch, { left, top, scale }, springOptions)
     }
   }
 
@@ -74,6 +77,7 @@ export class Animate extends React.Component<Props> {
         onTap={this.props.onTap}
         scale={this.switch.scale}
         left={this.switch.left}
+        top={this.switch.top}
       >
         {this.props.text}
       </AnimateFrame>
