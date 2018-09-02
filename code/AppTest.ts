6de879e495d604af388489a64adf93d04e7451ed
ruucm <ruucm.a@gmail.com>
2018-09-02 13:15:32 +0900

    rm unusings

--- code/AppTest.ts
+++ code/AppTest.ts
@@ -19,16 +19,6 @@ export const Animate: Override = () => {
   }
 }
 
-export const Scale: Override = () => {
-  return {
-    scale: data.scale,
-    onTap() {
-      data.scale.set(0.6)
-      animate.spring(data.scale, 1)
-    },
-  }
-}
-
 export const Trigger: Override = () => {
   return {
     onTap(type) {
@@ -43,55 +33,6 @@ export const Trigger: Override = () => {
     onMouseUp(type) {
       if (type == 'play') data.playingOnMouseUp = true
       else if (type == 'reverse') data.playingOnMouseUp = false
-    },
-  }
-}
-
-export const Rotate: Override = props => {
-  data.rotation.set(props.rotation)
-
-  return {
-    rotation: data.rotation,
-    onTap() {
-      animate.spring(data.rotation, data.rotation.get() + 90, {
-        tension: 250,
-        friction: 20,
-      })
-    },
-  }
-}
-
-export const Fade: Override = props => {
-  data.opacity.set(props.opacity)
-
-  return {
-    opacity: data.opacity,
-    onTap() {
-      animate.linear(data.opacity, 0, 0.2)
-    },
-  }
-}
-
-export const SwitchOutput: Override = () => {
-  return {
-    opacity: data.opacity,
-    rotation: data.rotation,
-  }
-}
-
-export const SwitchInput: Override = () => {
-  return {
-    onTap() {
-      const toggle = data.toggle
-      animate.spring(
-        { opacity: data.opacity, rotation: data.rotation },
-        {
-          opacity: toggle ? 0.5 : 1,
-          rotation: toggle ? 360 : 0,
-        },
-        { tension: 200, friction: 20 }
-      )
-      data.toggle = !toggle
     },
   }
 }
